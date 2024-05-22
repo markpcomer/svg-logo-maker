5cbec665d647137b5c3bd7ccde70704a2f869bbd
markpcomer <markpcomer@gmail.com>
2024-05-21 21:51:49 -0400

    Reorganizing code. Working on index.js inquirer.prompt

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-const CLI = require("./lib/cli.js");
+const inquirer = require("inquirer");
+const fs = require('fs');
+const { writeFile } = require(fs/promises);
+const { Triangle, Circle, Square } = require('./shapes.js');
+const SVG = require('./svg.js');
+const { error } = require("console");
 
-const cli = new CLI();
-
-cli.run();
+    inquirer
+            .prompt ([
+                {
+                    type: 'input',
+                    name: 'shapeText',
+                    message: "Please enter logo text here. Do not enter more than 3 characters.",
+                },
+                {
+                    type: 'input',
+                    name: 'textColor',
+                    message: "Enter what color you'd like for text color. Enter a color keyword or hexidecimal number.",
+                },
+                {
+                    type: 'checkbox',
+                    name: 'shapeForm',
+                    message: "Please select a shape.",
+                    options: ['Circle', 'Triangle', 'Square'],
+                },
+                {
+                    type: 'input',
+                    name: 'shapeColor',
+                    message: "Please enter your shape's color. Enter a color keyword or hexidecimal number.",
+                },
+            ])
+            .then((res) => {
+                if(res.shapeForm === 'Triangle'){
+                    const triangle  = new Triangle(res.shapeText, res.textColor, res.textColor);
+                    fs.writeFile('./examples/logo.svg', triangle.render(),
+                        (error) => {
+                            if(error){
+                                console.log("Error");
+                            }
+                        
+                        }) 
+                }
+            })
